ca623b5aa6157645d94dc80fcb9e61a1876375d9
jondrielle wilson <jondriellewilson@Roberts-MacBook-Pro.local>
2026-06-20 22:55:47 -0500

    Working on post request logic in vue

--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,18 @@ from fastapi import FastAPI
 from routes.habits import router
 from fastapi.middleware.cors import CORSMiddleware
 
-origins =[
-	
-]
-
 app = FastAPI()
 
+origins =[
+	"http://localhost:5173",
+]
 
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins = origins,
+	allow_credentials = True,
+	allow_methods = ["*"],
+	allow_headers = ["*"],
+)
 
 app.include_router(router)
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,44 @@
-<script setup></script>
+<script setup>
+import {ref,onMounted} from 'vue'
+
+const habits = ref([])
+
+const name = ref("")
+const description = ref("")
+
+async function getHabitList(){
+  try{
+    const response = await fetch("http://127.0.0.1:8000/");
+
+    if (!response.ok){
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    
+    console.log(result)
+  }catch(error){
+    console.error(error.message)
+  }
+}
+
+async function addHabit(){
+  console.log(name.value)
+  console.log(description.value)
+}
+
+onMounted(()=>{
+  habits = getHabitList()}
+)
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <input v-model="name" placeholder="Name"/>
+  <input v-model="description" placeholder="Description"/>
+
+  <button @click="addHabit" :name>
+    Add Habit
+  </button>
 </template>
 
 <style scoped></style>
