ae043556379f5e25d4a5c2f60a5421184b699fb5
jondrielle wilson <jondriellewilson@MacBookPro.home.local>
2026-07-24 22:32:43 -0500

    Adding render url link to CORS

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,8 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins =[
-	"http://localhost:5173"
+	"http://localhost:5173",
+	"https://habit-tracker-x6nr.onrender.com"
 ]
 
 app.add_middleware(
